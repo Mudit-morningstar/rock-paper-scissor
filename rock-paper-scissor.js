ac175b076ded3d7eed96c543f009d0f39aba6955
Mudit-morningstar <gupta.mudit2299@gmail.com>
2023-10-12 21:27:01 +0530

    init commit

---
+++ rock-paper-scissor.js
@@ -0,0 +1,76 @@
+const game = (() => {
+    let winCounter = 0;
+    let result = "";
+    for(let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let userSelection = prompt(`Please enter your choice (rock, paper or scissor)\n Win Counter: ${winCounter}\n ${result}`);
+        result =  playRound(userSelection, computerSelection);
+        if(result === win) {
+            winCounter+=1;
+            console.log(win);
+        } else if(result === lose) {
+            console.log(lose);
+        } else {
+            i--;
+            console.log(tie)
+        }
+    }
+
+    if(winCounter >= 3) {
+        console.log("Congratulations!!, You've won the game")
+    } else {
+        console.log("Alas!! You've lost the game")
+    }
+})
+
+
+const choices = ["rock", "paper", "scissor"];
+
+
+const win = "Wohoo!! You Won";
+const lose = "Oh Ho!! You lost";
+const tie = "Hey!! It's a tie";
+
+const getComputerChoice = (() => {
+    index = Math.floor(Math.random() * 3);
+    return choices[index];
+})
+
+const playRound = ((userChoice, computerChoice) => {
+    userSelectionClean = userChoice.toLowerCase().trim();
+
+    if(userSelectionClean === "rock") {
+        if(computerChoice === "rock"){
+            return tie;
+        }
+        else if(computerChoice === "paper"){
+            return lose;
+        } else {
+            return win
+        }
+    } else if(userSelectionClean === "paper") {
+        if(computerChoice === "rock"){
+            return win;
+        }
+        else if(computerChoice === "paper"){
+            return tie;
+        } else {
+            return lose
+        }
+    } else if(userSelectionClean === "scissor") {
+        if(computerChoice === "rock"){
+            return lose;
+        }
+        else if(computerChoice === "paper"){
+            return win;
+        } else {
+            return tie
+        }
+    } else {
+        return "Please select out of rock paper and scissor"
+    }
+})
+
+window.onload = game();
+
+
